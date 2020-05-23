48bf8d6d7e5110d31a03f1db115c1717d3eff0e2
Thịnh Nguyễn <44135441+ThinhNguyen100199@users.noreply.github.com>
2020-05-23 21:13:01 +0700

    Update index.php
    add link

--- index.php
+++ index.php
@@ -9,6 +9,13 @@
 		<link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,500,700&display=swap&subset=vietnamese" rel="stylesheet">
 		<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 		<script type="text/javascript" src="js/main.js"></script>
+		<link
+	href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+	rel="stylesheet"
+	/>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	</head>
 
 	<body>
